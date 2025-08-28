e454c0b8efc9126f4e529036a84885ab9015ded0
parthugare <parthugare607@gmail.com>
2025-08-28 18:17:01 +0530

    d

---
+++ ass1/Q1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int  n1,n2;
+	long  res;
+
+	
+	printf("Enter 2 numbers : ");
+	scanf("%d%d",&n1,&n2);
+
+
+	res=n1+n2;
+	printf("res=%d\n",res);
+
+
+	printf("res= %d\n",n1-n2,res);
+
+	printf("res= %d\n",n1*n2,res);
+
+return 0;
+
+}
+
+
